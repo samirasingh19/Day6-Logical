699f96c5ec617b59891c29eb695aecce9ff2636d
samirasingh19 <samira.singh021@gmail.com>
2023-03-21 21:43:47 +0530

    UC1

---
+++ Fibonassi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Fibonassi
+    {
+        public static void Fibo()
+        {
+            int firstnumber = 0, secondnumber = 1, nextnumber, numberofelements;
+            Console.WriteLine("Enter the number of elements : ");
+            numberofelements = Convert.ToInt32(Console.ReadLine());
+            if (numberofelements < 2)
+            {
+                Console.WriteLine("Please enter a value greater than two");
+            }
+            else
+            {
+                Console.WriteLine(firstnumber + "," + secondnumber);
+                for (int i = 2; i < numberofelements; i++)
+                {
+                    nextnumber = firstnumber + secondnumber;
+                    Console.WriteLine(nextnumber + " ");
+                    firstnumber = secondnumber;
+                    secondnumber = nextnumber;
+                }
+            }
+        }
+    }
+}
